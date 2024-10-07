5d1986da5bc01f7251aaeada3f0a82744c5a2559
Alex <bocandzo@mail.ru>
2024-10-07 19:32:30 +0300

    Comleted service

--- myapp/main.py
+++ myapp/main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+import pickle
 
 from fastapi import FastAPI, Body
 from fastapi.staticfiles import StaticFiles
@@ -11,6 +13,15 @@ logger = logging.getLogger(__name__)
 
 # load model
 model = Model()
+with open('myapp/model.pkl', 'rb') as f:
+    data = pickle.load(f)
+model.load_state_dict(data)
+
+# make symbol dict
+with open('emnist-balanced-mapping.txt', 'r') as file:
+    content = file.read()
+lines = content.strip().split('\n')
+labels_dict = {int(lines[i].split(' ')[0]): chr(int(lines[i].split(' ')[1])) for i in range(len(lines))}
 
 # app
 app = FastAPI(title='Symbol detection', docs_url='/docs')
@@ -19,8 +30,9 @@ app = FastAPI(title='Symbol detection', docs_url='/docs')
 @app.post('/api/predict')
 def predict(image: str = Body(..., description='image pixels list')):
     image = np.array(list(map(int, image[1:-1].split(','))))
-    pred = model.predict(image)
-    return {'prediction': pred}
+    x = torch.from_numpy(image.reshape(-1, 28 * 28).astype(np.float32))
+    pred = model(x)
+    return {'prediction': labels_dict[pred.argmax().item()]}
 
 # static files
 app.mount('/', StaticFiles(directory='static', html=True), name='static')
--- myapp/model.py
+++ myapp/model.py
@@ -1,11 +1,33 @@
 import os
 
+import numpy as np
+import torch
+import torch.nn as nn
 
-class Model:
-    def __init__(self):
-        model_path = os.path.join('myapp', 'model.pkl')
-        # your code here
 
+class Model(nn.Module):
+    def __init__(self, input_dim=784, output_dim=47):
+        super().__init__()
+        self.layer_1 = nn.Linear(input_dim, 128)
+        self.layer_2 = nn.Linear(128, output_dim)
+        self.dropout = nn.Dropout(0.3)
+        self.act = nn.ReLU()
+
+    def forward(self, x):
+        x = self.layer_1(x)
+        x = self.act(x)
+        x = self.dropout(x)
+        out = self.layer_2(x)
+        return out
+
+
+# class Model:
+#     def __init__(self):
+#         model_path = os.path.join('myapp', 'model.pth')
+#         self.model = torch.load(model_path)
+#         self.model.eval()
+#         # your code here
+#
     def predict(self, x):
         '''
         Parameters
@@ -15,6 +37,20 @@ class Model:
         Returns
         -------
         pred : str
-            Символ-предсказание 
+            Символ-предсказание
         '''
-        # your code here
+        x = torch.from_numpy(x.reshape(-1, 28 * 28).astype(np.float32))
+
+        with open('emnist-balanced-mapping.txt', 'r') as file:
+            content = file.read()
+
+        lines = content.strip().split('\n')
+        labels_dict = {int(lines[i].split(' ')[0]): chr(int(lines[i].split(' ')[1])) for i in range(len(lines))}
+
+        with torch.no_grad():
+            pred = self.model(x)
+
+        return labels_dict[pred.argmax().item()]
+
+
+
